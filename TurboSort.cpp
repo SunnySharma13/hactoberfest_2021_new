4c45b2cec89b25d15953f0beb423575afb6b942d
ajayraj47 <93249832+ajayraj47@users.noreply.github.com>
2021-10-27 13:42:12 +0530

    Create TurboSort.cpp

---
+++ TurboSort.cpp
@@ -0,0 +1,18 @@
+// problem link - https://www.codechef.com/problems/TSORT
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+    cin >> a;
+    int arr[a];
+    for(int i=0; i<a; i++){
+        cin >> arr[i];
+    }
+    sort(arr, arr+a);
+    for(int i=0; i<a; i++){
+        cout << arr[i] << endl;
+    }
+    return 0;
+}
